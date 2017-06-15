3aaf8be980a107c67244d8979e271c244a58352f
iwag <iwagame@gmail.com>
2017-06-14 23:49:13 -0700

    Create mock clients in test

--- server_test.go
+++ server_test.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"golang.org/x/net/context"
 	"google.golang.org/appengine/aetest"
 	"testing"
+	"errors"
 )
+
+type (
+	TestNormalClient struct{}
+	TestErrorClient struct{}
+)
+
+func (c TestNormalClient) GetRss(ctx context.Context, url string) (Rss, error) {
+	return Rss{}, nil
+}
+
+func (c TestErrorClient) GetRss(ctx context.Context, url string) (Rss, error) {
+	return Rss{}, errors.New("errrrorrrr")
+}
 
 func testNormal(t *testing.T) {
 
-	ctx, done, err := aetest.NewContext()
+	_, done, err := aetest.NewContext()
 	if err != nil {
 		t.Fatalf("aetest: %v", err)
 	}
 	defer done()
 }
-
